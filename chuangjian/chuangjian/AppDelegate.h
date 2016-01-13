e079a7302d4f1055b9f73319aa8120dcf04165bc
wzh <813158960@qq.com>
2016-01-13 15:19:32 +0800

    tjkjta

---
+++ chuangjian/chuangjian/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  chuangjian
+//
+//  Created by lanou on 16/1/13.
+//  Copyright © 2016年 wzh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ chuangjian/chuangjian/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  chuangjian
+//
+//  Created by lanou on 16/1/13.
+//  Copyright © 2016年 wzh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
